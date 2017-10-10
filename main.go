d84526d57de655a1d981f4e3ab0a2dafc851a249
Matthew Drumheller <MattDrummy@gmail.com>
2017-10-10 12:00:42 -0600

    added dynamic room creation to socket.io server

--- main.go
+++ main.go
@@ -1,116 +1,155 @@
 package main
 
 import (
-    "fmt"
-    "github.com/gin-gonic/gin"
-    "gopkg.in/mgo.v2"
-    "gopkg.in/mgo.v2/bson"
-    "net/http"
-    "time"
-    "strconv"
-    "io"
-    "os"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+  "log"
+
+	"github.com/gin-gonic/gin"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+  "github.com/gin-contrib/cors"
+  "github.com/googollee/go-socket.io"
 )
 
 type Student struct {
-  ID         bson.ObjectId `json:"id" bson:"_id,omitempty"`
-  Firstname  string
-  Lastname   string
-  Age        float64
-  Gender     string
-  Current    bool
-  Awesome    float64
-  Timestamp  int32
+	ID        bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	Firstname string
+	Lastname  string
+	Age       float64
+	Gender    string
+	Current   bool
+	Awesome   float64
+	Timestamp int32
 }
 
 func main() {
 
-  gin.DisableConsoleColor()
+	gin.DisableConsoleColor()
 
-  f,_ := os.Create("gin.log")
-  gin.DefaultWriter = io.MultiWriter(f)
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f)
 
-  session, err := mgo.Dial("localhost")
-  students := session.DB("student").C("students")
+	session, err := mgo.Dial("localhost")
+	students := session.DB("student").C("students")
+	if err != nil {
+		panic(err)
+	}
+
+	router := gin.Default()
+  router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:4200"},
+		AllowMethods:     []string{"PUT", "PATCH", "DELETE", "GET"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))  // router.Use(cors.New(config)) // adding cors needs to be done before you add router modes
+
+	router.GET("/all", func(c *gin.Context) {
+		var data []Student
+		students.Find(nil).All(&data)
+		c.JSON(http.StatusOK, gin.H{
+			"students": data,
+		})
+	})
+
+	router.POST("/new", func(c *gin.Context) {
+		firstName := c.PostForm("firstname")
+		lastName := c.PostForm("lastname")
+		age, _ := strconv.ParseFloat(c.PostForm("age"), 64)
+		gender := c.DefaultPostForm("gender", "N/A")
+		awesome, _ := strconv.ParseFloat(c.PostForm("awesome"), 64)
+		query := bson.M{"current": true}
+		change := bson.M{"$set": bson.M{"current": false}}
+		students.UpdateAll(query, change)
+		err = students.Insert(&Student{
+			Firstname: firstName,
+			Lastname:  lastName,
+			Age:       age,
+			Gender:    gender,
+			Awesome:   awesome,
+			Current:   true,
+			Timestamp: int32(time.Now().Unix()),
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Posted",
+		})
+	})
+
+	router.DELETE("/delete/:id", func(c *gin.Context) {
+		time, _ := strconv.Atoi(c.Param("id"))
+		err := students.Remove(bson.M{"timestamp": time})
+		if err != nil {
+			panic(err)
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Deleted",
+		})
+	})
+
+	router.PUT("/update/:id", func(c *gin.Context) {
+		firstName := c.PostForm("firstname")
+		lastName := c.PostForm("lastname")
+		age, _ := strconv.ParseFloat(c.PostForm("age"), 64)
+		gender := c.DefaultPostForm("gender", "N/A")
+		awesome, _ := strconv.ParseFloat(c.PostForm("awesome"), 64)
+		current := c.PostForm("current")
+		time, _ := strconv.Atoi(c.Param("id"))
+		update := bson.M{
+			"firstname": firstName,
+			"lastname":  lastName,
+			"age":       age,
+			"gender":    gender,
+			"awesome":   awesome,
+			"current":   current,
+		}
+		change := bson.M{"$set": update}
+		students.Update(bson.M{"timestamp": time}, change)
+
+		var data []Student
+		students.Find(nil).All(&data)
+		c.JSON(http.StatusOK, gin.H{
+			"students": data,
+		})
+	})
+
+  foo, err := socketio.NewServer(nil)
   if err != nil {
-    panic(err)
+    log.Fatal(err)
   }
 
-  router := gin.Default()
-
-  router.GET("/all", func(c *gin.Context) {
-    var data []Student
-    students.Find(nil).All(&data)
-    c.JSON(http.StatusOK, gin.H{
-      "students": data,
+  server.On("connection", func(so socketio.Socket) {
+    so.On("open", func(user [2]string){
+      fmt.Println(user[0] + " connected to " + user[1])
+      so.BroadcastTo(user[1], "message", user[0] + " connected to " + user[1])
+      so.Join(user[1])
+    })
+    so.On("message", func(msg [2]string) {
+      fmt.Printf("%+v\n", msg)
+      so.Emit("message", msg[1])
+      so.BroadcastTo(msg[0], "message", msg[1])
+    })
+    so.On("disconnection", func() {
+      fmt.Println("on disconnect")
     })
   })
-
-  router.POST("/new", func(c *gin.Context){
-    firstName := c.PostForm("firstname")
-    lastName := c.PostForm("lastname")
-    age, _ := strconv.ParseFloat(c.PostForm("age"), 64)
-    gender := c.DefaultPostForm("gender", "N/A")
-    awesome, _ := strconv.ParseFloat(c.PostForm("awesome"), 64)
-    query := bson.M{"current": true}
-    change := bson.M{"$set": bson.M{"current": false}}
-    students.UpdateAll(query, change)
-    err = students.Insert(&Student{
-        Firstname: firstName,
-        Lastname: lastName,
-        Age: age,
-        Gender: gender,
-        Awesome: awesome,
-        Current: true,
-        Timestamp: int32(time.Now().Unix()),
-      })
-    if err != nil {
-      panic(err)
-    }
-
-    c.JSON(http.StatusOK, gin.H{
-      "message": "Posted",
-    })
+  server.On("error", func(so socketio.Socket, err error) {
+    log.Println("error:", err)
   })
 
-  router.DELETE("/delete/:id", func(c *gin.Context) {
-    time, _ := strconv.Atoi(c.Param("id"))
-    err := students.Remove(bson.M{"timestamp": time})
-    if err != nil {
-      panic(err)
-    }
 
-    c.JSON(http.StatusOK, gin.H{
-      "message": "Deleted",
-    })
-  })
-
-  router.PUT("/update/:id", func(c *gin.Context)  {
-    firstName := c.PostForm("firstname")
-    lastName := c.PostForm("lastname")
-    age, _ := strconv.ParseFloat(c.PostForm("age"), 64)
-    gender := c.DefaultPostForm("gender", "N/A")
-    awesome, _ := strconv.ParseFloat(c.PostForm("awesome"), 64)
-    current := c.PostForm("current")
-    time, _ := strconv.Atoi(c.Param("id"))
-    update := bson.M{
-      "firstname": firstName,
-      "lastname": lastName,
-      "age": age,
-      "gender": gender,
-      "awesome": awesome,
-      "current": current,
-    }
-    change := bson.M{"$set": update}
-    students.Update(bson.M{"timestamp": time}, change)
-
-    var data []Student
-    students.Find(nil).All(&data)
-    c.JSON(http.StatusOK, gin.H{
-      "students": data,
-    })
-  })
-
+  router.GET("/socket.io/", gin.WrapH(foo))
+  router.POST("/socket.io/", gin.WrapH(foo))
   fmt.Println("server ready")
-  router.Run(":8080")
+  router.Run(":7000")
+
 }
